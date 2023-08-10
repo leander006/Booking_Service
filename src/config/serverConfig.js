46bf111117c665a333a18d26730b74b4b8ad0ed4
leander006 <leanderdsilva06@gmail.com>
2023-08-10 22:37:49 +0530

    Basic setup

---
+++ src/config/serverConfig.js
@@ -0,0 +1,5 @@
+require("dotenv").config();
+module.exports = {
+  PORT: process.env.PORT,
+  DB_SYNC: process.env.DB_SYNC,
+};
---
+++ src/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+
+const bodyParser = require("body-parser");
+
+const apiRoutes = require("./routes/index");
+const { PORT } = require("./config/serverConfig");
+
+const db = require("./models/index");
+
+const prepareAndStartServer = () => {
+  const app = express();
+
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+
+  app.use("/api", apiRoutes);
+
+  app.listen(PORT, async (req, res) => {
+    if (process.env.DB_SYNC) {
+      db.sequelize.sync({ alter: true });
+    }
+    console.log(`Server started on port ${PORT}`);
+  });
+};
+
+prepareAndStartServer();
